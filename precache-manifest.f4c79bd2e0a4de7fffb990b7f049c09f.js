7750bb3cf74a4e26517344c304cc46079702be3f
koustub manchiraju <koustub.247@gmail.com>
2020-04-10 23:48:34 -0400

    Updates

---
+++ precache-manifest.f4c79bd2e0a4de7fffb990b7f049c09f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e2f08f42c38f44fe4cb57d603d7c49",
+    "url": "/googlebooksreact/index.html"
+  },
+  {
+    "revision": "4237059e161fe9c531a1",
+    "url": "/googlebooksreact/static/js/2.1c10d3f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/googlebooksreact/static/js/2.1c10d3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8f6fd0bfb84ceff3c8",
+    "url": "/googlebooksreact/static/js/main.64c3fd0e.chunk.js"
+  },
+  {
+    "revision": "264ed86598a1cc2cd96a",
+    "url": "/googlebooksreact/static/js/runtime-main.567b2e16.js"
+  }
+]);
